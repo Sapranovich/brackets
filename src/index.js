a6fc87adbf9460fc331a961d30b2b6a12a0421ee
Andrey Sapranovich <SapranovichAndrey@yandex.ru>
2020-03-05 21:44:50 +0300

    feat:add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let array = [];
+    let k = str.length / 2;
+
+    while (k >= 0) {
+        for (let i = 0; i < bracketsConfig.length; i++) {
+            array = bracketsConfig[i][0].concat(bracketsConfig[i][1]);
+            while (str.indexOf(array) != -1) {
+                str = str.replace(array, "");
+            }
+        }
+        k--;
+    }
+    if (str.length == 0) {
+        return true;
+    } else return false;
+};
